fe34edf28e02befac6842f29c831b887833c892e
Jusxjc <198653346+jusxjc@users.noreply.github.com>
2026-02-19 18:19:22 +0000

    V1

--- tracker.py
+++ tracker.py
@@ -1,29 +1,58 @@
 import json
+def add_workout(): # Ask the user to enter a workout
+    exercise= input("Enter an exercise: ")
+    sets= int(input("Enter sets: "))
+    reps= int(input("Enter reps: "))
+    weight= (input("Enter weight (or bodyweight): "))
+    return (exercise,sets,reps,weight)
 
-exercise= input("Enter an exercise: ")
-sets= int(input("Enter sets: "))
-reps= int(input("Enter reps: "))
-weight= (input("Enter weight (or bodyweight): "))
+def output_workout_added(exercise,sets,reps,weight): # Print the new workout added
+    print("Workout added:")
+    print("Exercise:", exercise)
+    print("Sets:", sets)
+    print("Reps:", reps)
+    print("Weight:", weight)
+      
+def create_workout_dict(exercise,sets,reps,weight): # Adds the new workout to a list of workouts via dictionary
+    workout_input= {"Exercise": exercise,
+            "Sets": sets,
+            "Reps": reps,
+            "Weight": weight
+            }
+    return (workout_input)
 
-print("Workout added:")
-print("Exercise:", exercise)
-print("Sets:", sets)
-print("Reps:", reps)
-print("Weight:", weight)
-workouts= []
-workout_input= {"Exercise": exercise,
-          "Sets": sets,
-          "Reps": reps,
-          "Weight": sets
-          }
-with open("workouts.json","r") as f:
-    json_data= json.load(f)
-    workouts.extend(json_data)
-    workouts.append(workout_input)
+def get_workout_list(workout_input): # Reads the JSON file and adds all workouts to a list
+    try:
+        with open("workouts.json","r") as f:
+            json_data= json.load(f)
+            json_data.append(workout_input)
+            return json_data
+    except FileNotFoundError:
+         print("File not found")
+         return [] # Returns an empty list to prevent errors when updating workout list
+    except:
+         print("An error occurred not relating to file found")
+         return []
 
-    print(workouts)
+def update_workout_list(workouts): # Updates the stored workouts in the JSON file
+    try:
+        with open("workouts.json","w") as f:
+                json.dump(workouts,f, indent=4)
+    except FileNotFoundError:
+         print ("File not found error")
+    except:
+         print("An error occurred")
+    
 
 
+def main(): #Main 
+      print("Welcome to Gym tracker")
+      exercise,sets,reps,weight= add_workout()
+      output_workout_added(exercise,sets,reps,weight)
+      workout_input= create_workout_dict(exercise,sets,reps,weight)
+      workouts = get_workout_list(workout_input)
+      update_workout_list(workouts)
+      
 
-with open("workouts.json","w") as f:
-        json.dump(workouts,f, indent=4)
+
+main()
