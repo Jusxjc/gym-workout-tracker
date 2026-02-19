8651deae19af365a9a979c1a2a3b60cd677265a9
Jusxjc <198653346+jusxjc@users.noreply.github.com>
2026-02-19 12:58:43 +0000

    Add JSON file read write

--- tracker.py
+++ tracker.py
@@ -1,3 +1,5 @@
+import json
+
 exercise= input("Enter an exercise: ")
 sets= int(input("Enter sets: "))
 reps= int(input("Enter reps: "))
@@ -8,3 +10,20 @@ print("Exercise:", exercise)
 print("Sets:", sets)
 print("Reps:", reps)
 print("Weight:", weight)
+workouts= []
+workout_input= {"Exercise": exercise,
+          "Sets": sets,
+          "Reps": reps,
+          "Weight": sets
+          }
+with open("workouts.json","r") as f:
+    json_data= json.load(f)
+    workouts.extend(json_data)
+    workouts.append(workout_input)
+
+    print(workouts)
+
+
+
+with open("workouts.json","w") as f:
+        json.dump(workouts,f, indent=4)
